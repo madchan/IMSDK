4746a0aacfb8fb3b000feee2e9adf9e8e781b2fa
chenf <chenf@qqty.com>
2021-04-13 15:40:56 +0800

    chore:改用ComposingBuilds形式管理SupportLib依赖库

---
+++ lib_plugins/src/main/java/com/madchan/imsdk/lib/plugins/Deps.kt
@@ -0,0 +1,15 @@
+package com.madchan.imsdk.lib.plugins
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class Deps : Plugin<Project> {
+
+    override fun apply(project: Project) {
+    }
+
+    companion object {
+        const val LIB_BASE = "com.litalk.supportlib:lib_base:1.0.0-alpha01"
+        const val LIB_NETWORK = "com.litalk.supportlib:lib_network:1.0.0-alpha01"
+    }
+}
